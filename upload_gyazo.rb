20dd02cb3ae11ae25852cfbc25f834acc1d9b713
Toshiyuki Masui <masui@pitecan.com>
2020-03-16 20:04:03 +0900

    ファイルの作成時刻(mtime)をGyazoにセットする

--- upload_gyazo.rb
+++ upload_gyazo.rb
@@ -3,6 +3,7 @@
 # PDFやJPEGなどをGyazoにアップロードする
 #
 # JPEGの場合はタイムスタンプを保持する
+# 古い画像の場合は作成時刻を利用するべきだろうか?
 #
 
 require 'exifr/jpeg'
@@ -14,17 +15,18 @@ gyazo = Gyazo::Client.new access_token: gyazo_token
 urls = []
 ARGV.each { |file|
   if File.exist?(file)
+    t = File.mtime(file)
     if file =~ /\.pdf$/i
       # 画像にしてGyazoにアップロード
       STDERR.puts "upload #{file} to Gyazo..."
       system "convert -density 300 '#{file}[0]' /tmp/upload_gyazo.png"
-      res = gyazo.upload imagefile: '/tmp/upload_gyazo.png'
+      res = gyazo.upload imagefile: '/tmp/upload_gyazo.png', created_at: t
       urls << res[:permalink_url]
       sleep 1
     elsif file =~ /\.(jpg|jpeg)$/i
       # JPEG
       STDERR.puts "upload #{file} to Gyazo..."
-      t = Time.now
+      # t = Time.now
       begin
         exif = EXIFR::JPEG.new(file)
         t = exif.date_time if exif.date_time.to_s != ''
@@ -36,7 +38,7 @@ ARGV.each { |file|
     elsif file =~ /\.(gif|png)$/i
       # その他の画像
       STDERR.puts "upload #{file} to Gyazo..."
-      res = gyazo.upload imagefile: file
+      res = gyazo.upload imagefile: file, created_at: t
       urls << res[:permalink_url]
       sleep 1
     end
