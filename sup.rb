af5870966b35f1075725355b01e19043feda9e87
Toshiyuki Masui <masui@pitecan.com>
2020-04-17 17:16:54 +0900

    supを修正して、GyazoページにS3ページのURLをコメントとして書くようにした

--- sup.rb
+++ sup.rb
@@ -6,10 +6,32 @@
 # S3とかへのアップロードは upload コマンド
 # Gyazoへのアップロードは upload_gyazo
 #
+# Scrapboxにページを作らない場合は -n オプション
+#
+
+require 'gyazo'
+require 'digest/md5'
+
+make_scrapbox_page = true
+if ARGV[0] == "-n"
+  make_scrapbox_page = false
+  ARGV.shift
+end
+
+file = ARGV[0]
+gyazourl = ARGV[1]
+gyazourl =~ /[0-9a-f]{32}/
+gyazoid = $&
+
+if !file || (gyazourl && !gyazoid)
+  STDERR.puts "% sup document [GyazoURL]"
+  exit
+end
+
+gyazo_token = ENV['GYAZO_TOKEN'] # .bash_profileに書いておく
+gyazo = Gyazo::Client.new access_token: gyazo_token
 
 project = "masui-files" # Scrapboxプロジェクト名
-
-require 'digest/md5'
 
 if `which open` != ""
   open = "open"
@@ -20,34 +42,44 @@ else
   exit
 end
 
-ARGV.each { |file|
-  exit unless File.exist?(file)
+exit unless File.exist?(file)
 
-  hash = Digest::MD5.new.update(File.read(file)).to_s
-  if file =~ /^.*(\.\w+)$/ then
-    ext = $1
-  end
-    
-  urls = []
-  
-  # S3などにアップロード
-  STDERR.puts "upload '#{file}'"
-  s3url = `upload '#{file}'`.chomp
-  STDERR.puts s3url
-  urls << s3url
+hash = Digest::MD5.new.update(File.read(file)).to_s
+if file =~ /^.*(\.\w+)$/ then
+  ext = $1
+end
 
-  # Gyazoにアップロード
-  STDERR.puts "upload_gyazo '#{file}'"
-  gyazourl = `upload_gyazo '#{file}'`.chomp
-  if gyazourl != ''
-    STDERR.puts gyazourl
-    urls << gyazourl
-  end
+# S3にアップロード
+STDERR.puts "upload '#{file}'"
+s3url = `upload '#{file}'`.chomp
+STDERR.puts s3url
 
-  title = Time.now.strftime('%Y%m%d%H%M%S')
+tmpimage = nil
+if gyazourl
+  # 既に登録されているGyazo画像を利用
+  #
+  # 登録されてるGyazoデータを取得
+  #
+  res = gyazo.image image_id: gyazoid
+  url = "https://gyazo.com/#{gyazoid}.#{res[:type]}"
+  tmpimage = "/tmp/tmpimage#{$$}.#{res[:type]}"
+  cmd = "wget -q #{url} -O #{tmpimage}"
+  system cmd
+  #
+  # Gyazo.comのデータ削除
+  #
+  gyazo.delete image_id: gyazoid
+end
 
-  scrapboxurl = "http://scrapbox.io/#{project}/#{title}?body=[#{urls.join(' ')}]%0d"
+# Gyazoにアップロード
+STDERR.puts "upload_gyazo '#{tmpimage}' #{s3url}"
+gyazourl = `upload_gyazo '#{tmpimage}' #{s3url}`.chomp
 
+if make_scrapbox_page
   # Scrapboxページ作成
+  title = Time.now.strftime('%Y%m%d%H%M%S')
+  scrapboxurl = "http://scrapbox.io/#{project}/#{title}?body=[#{s3url} #{gyazourl}]%0d"
   system "#{open} '#{scrapboxurl}'"
-}
+end
+
+File.delete tmpimage if tmpimage
--- upload_gyazo.rb
+++ upload_gyazo.rb
@@ -21,42 +21,40 @@ end
 gyazo_token = ENV['GYAZO_TOKEN'] # .bash_profileに書いておく
 gyazo = Gyazo::Client.new access_token: gyazo_token
 
-urls = []
-ARGV.each { |file|
-  if File.exist?(file)
-    t = File.mtime(file)
-    if file =~ /\.pdf$/i
-      # 画像にしてGyazoにアップロード
-      STDERR.puts "upload #{file} to Gyazo..."
-      system "convert -density 300 '#{file}[0]' /tmp/upload_gyazo.png"
-      res = gyazo.upload imagefile: '/tmp/upload_gyazo.png', created_at: t
-      urls << res[:permalink_url]
-      sleep 1
-    elsif file =~ /\.(jpg|jpeg)$/i
-      # JPEG
-      STDERR.puts "upload #{file} to Gyazo..."
-      # t = Time.now
-      begin
-        exif = EXIFR::JPEG.new(file)
-        t = exif.date_time if exif.date_time.to_s != ''
-      rescue
-      end
-      res = gyazo.upload imagefile: file, created_at: t
-      urls << res[:permalink_url]
-      sleep 1
-    elsif file =~ /\.(gif|png)$/i
-      # その他の画像
-      STDERR.puts "upload #{file} to Gyazo..."
-      res = gyazo.upload imagefile: file, created_at: t
-      urls << res[:permalink_url]
-      sleep 1
-    end
-  end
-}
+file = ARGV[0]
+desc = ARGV[1]
 
-urls.each { |url|
-  `echo #{url} | #{pbcopy}`
-  puts url
-}
+exit unless File.exist?(file)
+
+t = File.mtime(file)
+if file =~ /\.pdf$/i
+  # 画像にしてGyazoにアップロード
+  STDERR.puts "upload #{file} to Gyazo..."
+  system "convert -density 300 '#{file}[0]' /tmp/upload_gyazo.png"
+  res = gyazo.upload imagefile: '/tmp/upload_gyazo.png', created_at: t, desc: desc
+  url = res[:permalink_url]
+  sleep 1
+elsif file =~ /\.(jpg|jpeg)$/i
+  # JPEG
+  STDERR.puts "upload #{file} to Gyazo..."
+  # t = Time.now
+  begin
+    exif = EXIFR::JPEG.new(file)
+    t = exif.date_time if exif.date_time.to_s != ''
+  rescue
+  end
+  res = gyazo.upload imagefile: file, created_at: t, desc: desc
+  url = res[:permalink_url]
+  sleep 1
+elsif file =~ /\.(gif|png)$/i
+  # その他の画像
+  STDERR.puts "upload #{file} to Gyazo..."
+  res = gyazo.upload imagefile: file, created_at: t, desc: desc
+  url = res[:permalink_url]
+  sleep 1
+end
+
+system "echo #{url} | #{pbcopy}"
+puts url
 
 
